cc4de58767ce279953815281379a97412e45d4fb
Emmanuel Ethelbert <103297116+emmanuelethelbert04@users.noreply.github.com>
2025-08-19 00:47:23 -0700

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@acme/ui/dist/**/*.{js,ts,jsx,tsx}"
 	],
 	prefix: "",
 	theme: {
